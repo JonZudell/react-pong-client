382458cb07920cb81d12129be58c2f545b78fbd7
Jon Zudell <jonathon.zudell@gmail.com>
2024-03-30 19:47:11 -0500

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,215 +1,16 @@
 import React from "react";
-import Modal from "./components/Modal";
-import Paddle from "./components/Paddle";
-import Ball from "./components/Ball";
 function App() {
-  const [ready, setReady] = React.useState<boolean>(false);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [win, setWin] = React.useState<boolean>(false);
-  const [lose, setLose] = React.useState<boolean>(false);
-  const [modalOpen, setModalOpen] = React.useState<boolean>(true);
-  const [name, setName] = React.useState<string>("Anonymous");
-
   const [socket, setSocket] = React.useState<WebSocket | null>(null);
-  const [started, setStarted] = React.useState<boolean>(false);
-  const [reset, setReset] = React.useState<boolean>(false);
-
-  const [playerAName, setPlayerAName] = React.useState<string | null>(null);
-  const [playerBName, setPlayerBName] = React.useState<string | null>(null);
-
-  const [paddleA, setPaddleA] = React.useState<Paddle | null>(null);
-  const [paddleB, setPaddleB] = React.useState<Paddle | null>(null);
-
-  const [scoreA, setScoreA] = React.useState<number | null>(null);
-  const [scoreB, setScoreB] = React.useState<number | null>(null);
-
-  const [ball, setBall] = React.useState<Ball | null>(null);
+ 
+  const onmessage = function (evt: MessageEvent) {
+    console.log(evt.data);
+    console.log(evt.type)
+  };
 
   const canvas = React.useRef<HTMLCanvasElement>(null);
   React.useEffect(() => {
-    draw();
-  }, [
-    paddleA,
-    paddleB,
-    ball,
-    scoreA,
-    scoreB,
-    win,
-    lose,
-    playerAName,
-    playerBName,
-    loading,
-    started,
-  ]);
-  React.useEffect(() => {
-    document.addEventListener("keyup", function (event) {
-      if (event.key === "w") {
-        if (socket) {
-          socket.send(
-            '{"type" : "input", "input" : "up", "status" : "released"}'
-          );
-        }
-      }
-      if (event.key === "s") {
-        if (socket) {
-          socket.send(
-            '{"type" : "input", "input" : "down", "status" : "released"}'
-          );
-        }
-      }
-    });
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "w") {
-        if (socket) {
-          socket.send('{"type" : "input", "input" : "up", "status" : "down"}');
-        }
-      }
-      if (event.key === "s") {
-        if (socket) {
-          socket.send(
-            '{"type" : "input", "input" : "down", "status" : "down"}'
-          );
-        }
-      }
-    });
-  });
-  const draw = () => {
-    if (canvas.current) {
-      const ctx = canvas.current.getContext("2d");
-      if (ctx) {
-        ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
-        if (loading) {
-          const loadingText = "Loading...";
-          const loadingTextWidth = ctx.measureText(loadingText).width;
-          ctx.font = "30px Arial";
-          ctx.fillStyle = "white";
-          ctx.fillText(
-            loadingText,
-            canvas.current.width / 2 - loadingTextWidth / 2,
-            canvas.current.height / 2
-          );
-        }
-        if (started) {
-          if (paddleA) {
-            paddleA.draw(ctx);
-          }
-          if (paddleB) {
-            paddleB.draw(ctx);
-          }
-          if (ball) {
-            ball.draw(ctx);
-          }
-          if (scoreA !== null && scoreB !== null) {
-            ctx.font = "30px Arial";
-            ctx.fillStyle = "white";
-            ctx.fillText(scoreA.toString(), canvas.current.width / 2 - 50, 50);
-            ctx.fillText(scoreB.toString(), canvas.current.width / 2 + 25, 50);
-          }
-          if (playerAName && playerBName) {
-            ctx.font = "20px Arial";
-            ctx.fillStyle = "white";
-            ctx.fillText(playerAName, 20, 50);
-            const playerBNameWidth = ctx.measureText(playerBName).width;
-            ctx.fillText(
-              playerBName,
-              canvas.current.width - playerBNameWidth - 20,
-              50
-            );
-          }
-        }
-        if (win) {
-          ctx.font = "50px Arial";
-          ctx.fillStyle = "white";
-          const winText = "You Win!";
-          const winTextWidth = ctx.measureText(winText).width;
-          ctx.fillText(
-            winText,
-            canvas.current.width / 2 - winTextWidth / 2,
-            canvas.current.height / 2
-          );
-        }
-        if (lose) {
-          ctx.font = "50px Arial";
-          ctx.fillStyle = "white";
-          const loseText = "You Lose!";
-          const loseTextWidth = ctx.measureText(loseText).width;
-          ctx.fillText(
-            loseText,
-            canvas.current.width / 2 - loseTextWidth / 2,
-            canvas.current.height / 2
-          );
-        }
-      }
-    }
-  };
-  const onmessage = function (evt: MessageEvent) {
-    var messages = evt.data.split("\n");
-    for (var i = 0; i < messages.length; i++) {
-      var message = JSON.parse(messages[i]);
-      if (message["type"] === "gamestate") {
-        setPaddleA(
-          new Paddle(
-            message.game.PlayerA.width,
-            message.game.PlayerA.height,
-            message.game.PlayerA.x,
-            message.game.PlayerA.y
-          )
-        );
-        setPaddleB(
-          new Paddle(
-            message.game.PlayerB.width,
-            message.game.PlayerB.height,
-            message.game.PlayerB.x,
-            message.game.PlayerB.y
-          )
-        );
-        setBall(
-          new Ball(
-            message.game.Ball.radius,
-            message.game.Ball.x,
-            message.game.Ball.y
-          )
-        );
-        setScoreA(message.game.scoreA);
-        setScoreB(message.game.scoreB);
-      } else if (message["type"] === "begin") {
-        setPlayerAName(message.playerAName);
-        setPlayerBName(message.playerBName);
-        setLoading(false);
-        setStarted(true);
-      } else if (message["type"] === "win") {
-        setStarted(false);
-        setWin(true);
-      } else if (message["type"] === "lose") {
-        setStarted(false);
-        setLose(true);
-      } else if (message["type"] === "reset") {
-        console.log("reset");
-        setWin(false);
-        setLose(false);
-        setReset(true);
-        setReady(true);
-      }
-    }
-  };
-
-  React.useEffect(() => {
-    console.log("reset called");
-    if (socket && ready) {
-      console.log(reset);
-      const message = {
-        type: "ready",
-        player: name,
-      };
-      socket.send(JSON.stringify(message));
-      setModalOpen(false);
-      setLoading(true);
-    }
-  }, [ready, reset]);
-
-  React.useEffect(() => {
     // Code to execute when start is true
-    let socket = new WebSocket("ws://localhost:3000/upgrade");
+    let socket = new WebSocket("ws://0.0.0.0:3000/upgrade");
 
     socket.onmessage = onmessage;
     socket.onopen = function () {};
@@ -229,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      <Modal
+      {/* <Modal
         open={modalOpen}
         onClose={() => {
           setModalOpen(false);
@@ -248,8 +49,8 @@ function App() {
         >
           Start
         </button>
-      </Modal>
-      <canvas className="Game" ref={canvas} width={1000} height={750}></canvas>
+      </Modal> 
+      <canvas className="Game" ref={canvas} width={1000} height={750}></canvas>*/}
     </div>
   );
 }
